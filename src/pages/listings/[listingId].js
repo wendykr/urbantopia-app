c1651926286b19c7c7a9b988c7d77c1184b28795
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-02 13:30:35 +0200

    feat: import useRouter into DetailPage

--- src/pages/listings/[listingId].js
+++ src/pages/listings/[listingId].js
@@ -1,3 +1,10 @@
+import { useRouter } from "next/router";
+
 export default function DetailPage() {
+  const router = useRouter();
+
+  const { listingId } = router.query;
+
+  console.log("listingId", listingId);
   return <div>DetailPage</div>;
 }
