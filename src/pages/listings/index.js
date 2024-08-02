7f73bf76389d796ab7bad1787743e28856de8540
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-02 12:25:02 +0200

    feat: create useListingsData for fetch data

--- src/pages/listings/index.js
+++ src/pages/listings/index.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
+
 import Header from "@/components/Header/Header";
 import ListingItem from "@/components/ListingItem/ListingItem";
 import SearchFilters from "@/components/SearchFilters/SearchFilters";
 
 const items = [1, 2, 3, 4, 5];
 
+const useListingsData = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("/listings.json");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+  return data;
+};
+
 export default function SearchPage() {
+  const listings = useListingsData();
+
+  console.log("listings", listings);
   return (
     <div>
       <Header />
