4e74b51b5c6f2eeb46b3cacd6e4ff4d2dfa8b92c
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-02 10:57:26 +0200

    ref: restyle components

--- src/components/SearchFilters/SearchFilters.jsx
+++ src/components/SearchFilters/SearchFilters.jsx
@@ -3,14 +3,14 @@ import FilterSelect from "../FilterSelect/FilterSelect";
 
 export default function SearchFilters() {
   return (
-    <div>
-      <div className="flex gap-2">
+    <div className="bg-slate-100 p-8 mb-4">
+      <div className="flex gap-2 justify-center mb-4">
         <input
           type="text"
           placeholder="Type here"
-          className="input input-bordered input-primary w-full max-w-xs"
+          className="input input-bordered input-primary w-full max-w-4xl"
         />
-        <button className="btn">Button</button>
+        <button className="btn">Search</button>
       </div>
       <div className="grid grid-cols-3">
         <FilterSelect label="Location" />
