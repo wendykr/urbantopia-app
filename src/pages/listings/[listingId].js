595d16fda833fc93659d471416321c69796dac53
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-02 13:45:07 +0200

    feat: filter fetch data into DetailPage

--- src/pages/listings/[listingId].js
+++ src/pages/listings/[listingId].js
@@ -1,10 +1,36 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+const useListingData = (listingId) => {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch("/listings.json");
+    const responseData = await response.json();
+    setData(
+      responseData.filter((listing) => listing.id === Number(listingId))?.[0]
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [listingId]);
+
+  return data;
+};
 
 export default function DetailPage() {
   const router = useRouter();
 
   const { listingId } = router.query;
 
-  console.log("listingId", listingId);
-  return <div>DetailPage</div>;
+  const listing = useListingData(listingId);
+
+  console.log("listing", listing);
+  return (
+    <div>
+      DetailPage
+      <p>Listing id: {listingId}</p>
+    </div>
+  );
 }
