b37700c180d20dbee49a9b790b6d7a7295a09d92
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-20 12:35:46 +0200

    feat: filter search value

--- src/hooks/useListingsData.jsx
+++ src/hooks/useListingsData.jsx
@@ -6,15 +6,25 @@ export default function useListingsData(filters) {
   const [data, setData] = useState([]);
 
   const fetchData = async () => {
-    let query = supabase
-      .from("listings")
-      .select()
-      .gte("price", filters.min_price ?? 0)
-      .lte("price", filters.max_price ?? Infinity)
-      .gte("year_built", filters.year ?? 0)
-      .gte("area", filters.area ?? 0)
-      .gte("number_of_bedrooms", filters.bedrooms ?? 0)
-      .gte("number_of_bathrooms", filters.bathrooms ?? 0);
+    let query = supabase.from("listings").select();
+    if (filters.min_price) {
+      query = query.gte("price", filters.min_price ?? 0);
+    }
+    if (filters.max_price) {
+      query = query.lte("price", filters.max_price ?? Infinity);
+    }
+    if (filters.year_built) {
+      query = query.gte("year_built", filters.year ?? 0);
+    }
+    if (filters.area) {
+      query = query.gte("area", filters.area ?? 0);
+    }
+    if (filters.bedrooms) {
+      query = query.gte("number_of_bedrooms", filters.bedrooms ?? 0);
+    }
+    if (filters.bathrooms) {
+      query = query.gte("number_of_bathrooms", filters.bathrooms ?? 0);
+    }
 
     if (filters.location) {
       query = query.ilike("location", `%${filters.location}%`);
@@ -22,6 +32,12 @@ export default function useListingsData(filters) {
 
     if (filters.property_type) {
       query = query.ilike("property_type", `%${filters.property_type}%`);
+    }
+
+    if (filters.search) {
+      query = query.or(
+        `title.ilike.%${filters.search}%, description.ilike.%${filters.search}%, location.ilike.%${filters.search}%, property_type.ilike.%${filters.search}%`
+      );
     }
 
     const { data: listings, error } = await query;
