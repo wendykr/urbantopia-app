7842a3c5e745467f8adcdb497c6aa0750caf3978
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-21 11:16:37 +0200

    ref: import listings into index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,8 @@
 import { Inter } from "next/font/google";
+import Listings from "../pages/listings";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <button className="btn">Button</button>
-    </main>
-  );
+  return <Listings />;
 }
