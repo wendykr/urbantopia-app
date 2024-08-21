17f8dbbfe2adf082b213291f30cbcc1895240327
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-21 12:58:30 +0200

    feat: import head title

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Head>
+        <title>Urbantopia</title>
+      </Head>
       <Header />
 
       {children}
