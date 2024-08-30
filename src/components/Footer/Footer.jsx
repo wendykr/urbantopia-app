1b8960a43d3668e6c2bf933bb0a5c4187a68d155
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-30 15:18:15 +0200

    ref: up text into foot

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
 export default function Footer() {
   return (
     <footer className="text-center bg-slate-700 text-white py-4">
-      &copy; 2024 Urbantopia
+      <p>
+        &copy; 2024{" "}
+        <a
+          className="underline hover:no-underline"
+          href="https://github.com/wendykr/"
+        >
+          Vendula Krajíčková
+        </a>
+      </p>
+      <p>
+        Praktický projekt z kurzu{" "}
+        <a
+          className="underline hover:no-underline"
+          href="https://skillmea.cz/online-kurzy/nextjs-zaklady"
+        >
+          Základy Next.js
+        </a>
+      </p>
     </footer>
   );
 }
